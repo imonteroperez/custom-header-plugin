26bd2d06aa85660c68abdeda58f4be5ee8dd3582
imonteroperez <imontero@cloudbees.com>
2021-11-10 11:20:50 +0100

    Apply some feedback

--- src/main/java/org/jenkinsci/plugins/custom/header/CustomHeader.java
+++ src/main/java/org/jenkinsci/plugins/custom/header/CustomHeader.java
@@ -4,7 +4,7 @@ import hudson.Extension;
 import jenkins.views.Header;
 
 @Extension(optional = true, ordinal = Integer.MAX_VALUE)
-public class CustomHeader extends Header{
+public class CustomHeader implements Header{
 
     @Override
     public boolean isHeaderEnabled() {
