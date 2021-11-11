ed675ea48a73efd7a1da295fd258b32a08fc5350
imonteroperez <imontero@cloudbees.com>
2021-11-11 11:11:15 +0100

    Add label programatically

--- src/main/java/org/jenkinsci/plugins/custom/header/CustomHeader.java
+++ src/main/java/org/jenkinsci/plugins/custom/header/CustomHeader.java
@@ -8,8 +8,14 @@ public class CustomHeader implements Header{
 
     @Override
     public boolean isHeaderEnabled() {
-        boolean isDisabled = "true".equalsIgnoreCase(System.getenv("CUSTOM_HEADER_DISABLE"));
+        // Disable/enable the header based on an ENV var and/or system property
+        boolean isDisabled = System.getProperty(CustomHeader.class.getName() + ".disable") != null ?
+                "true".equalsIgnoreCase(System.getProperty(CustomHeader.class.getName() + ".disable")) :
+                "true".equalsIgnoreCase(System.getenv("CUSTOM_HEADER_DISABLE"));
         return !isDisabled;
     }
-
+    
+    public static String getHeaderLabel(){
+        return "Hello World!"; // This label content could be retrieved programatically
+    }
 }
